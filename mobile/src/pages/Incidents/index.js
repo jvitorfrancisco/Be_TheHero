a808c6c738537928a53d766ac1f07d015e667a80
João Vitor de Francisco <jvitorjuca@gmail.com>
2020-07-20 19:13:36 -0300

    Mobile: primeiras chamadas na API

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { View, FlatList , Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+import api from '../../services/api';
+
+import styles from './styles';
+import logoImg from '../../assets/logo.png';
+import { Feather } from '@expo/vector-icons'
+
+export default function Incidents(){
+    const navigation = useNavigation();
+    const [incidents, setIncidents] = useState([]);
+
+    function navigateToDetail() {
+        navigation.navigate('Detail');
+    }
+
+    async function loadIncidents() {
+        const response = await api.get('incidents');
+        setIncidents(response.data);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, [])
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+                </Text>
+            </View>
+
+            <Text style={styles.title}>Bem-vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia!</Text>
+
+            <FlatList 
+                style={styles.incidentList}
+                data={incidents}
+                keyExtractor={incident => String(incident.id)}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item: incident }) => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                        <Text style={styles.incidentProperty}>Titulo:</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
+                        
+                        <Text style={styles.incidentProperty}>Valor:</Text>
+                        <Text style={styles.incidentValue}>{incident.value}</Text>
+
+                        <TouchableOpacity 
+                            style={styles.detailsButton}
+                            onPress={navigateToDetail}
+                        >
+                            <Text style={styles.detailsButtonText}>
+                                Ver mais detalhes
+                            </Text>
+                            <Feather name="arrow-right" size={16} color="#e02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
---
+++ mobile/src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import Constants from "expo-constants";
+
+const { manifest } = Constants;
+
+const api = axios.create({
+    baseURL: `http://${manifest.debuggerHost.split(':').shift()}:3333`
+});
+
+export default api;
