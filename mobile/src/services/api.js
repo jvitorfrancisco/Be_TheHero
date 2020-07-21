54589c72c66a8fcffb46bbdbf4851193adfcaebc
João Vitor de Francisco <jvitorjuca@gmail.com>
2020-07-21 12:59:40 -0300

    External ipaddress service Requests

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'http://187.63.82.117:62330',
+    // baseURL: 'http://10.5.60.105:3333',
+    // baseURL: 'http://localhost:3333',
     // baseURL: 'http://89cdbbbad3a3.ngrrok.io',
     headers: {'Access-Control-Allow-Origin': '*'}
 });
